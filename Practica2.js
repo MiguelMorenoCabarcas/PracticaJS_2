a11a912ea6534da5e86a4c22523346efc7869e76
MiguelMorenoCabarcas <mamoreno2003@gmail.com>
2022-01-28 10:25:41 -0500

    Subida de ejercicios 1-3

---
+++ Practica2.js
@@ -0,0 +1,44 @@
+
+
+//EJERCICIO 1
+
+ /* for (let i = 0  ; i <= 9 ; i++) {   
+    if (i==0 || i==9) {
+        console.log(`*****************`)
+    }else{console.log(`*               *`) }
+}  */
+
+//EJERCICIO 2
+
+/*   let cantidad = 20 
+ 
+
+for (let i = 1; i <= cantidad ; i++) {
+    
+    
+    if (i%15==0) {
+        console.log(`TzStrike`)
+    }else if (i%3==0) {
+        console.log(`Tz`)
+    }else if (i%5==0) {
+        console.log(`Strike`)
+    }else{
+        console.log(i)
+    }
+    
+} */  
+
+//EJERCICIO 3
+
+/* let tabla = 5
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${tabla} x ${i} = ${tabla*i}` )
+    
+} */
+
+//EJERCICIO AVANZADO
+
+/* let texto = `medium`
+let texto_modificado = texto.slice(4)
+console.log(texto_modificado) */
